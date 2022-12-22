60699d054a6f4aa94806486d8c907dc20dd80737
Tomwazz <tomvas@email.cz>
2022-12-22 19:03:32 +0100

    init

--- ToDoList/js/script.js
+++ ToDoList/js/script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    document.querySelector('#addBtn').onclick = newElement;
-    document.querySelector('#error').onclick = errorOff;
+    document.querySelector("#addBtn").onclick = newElement;
+    document.querySelector("#error").onclick = errorOff;
+    console.log("DOM loaded.")
 
     let todos = [];
 
@@ -10,9 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
             this.name = name;
             this.completed = false;
         }
-
     }
-    gettodos();
+
+    getTodos();
 
     function addNewTodo(name) {
         let temp = new Todo(name);
@@ -26,30 +27,27 @@ document.addEventListener("DOMContentLoaded", () => {
     function getTodos() {
         todos = JSON.parse(localStorage.getItem("todos"));
 
-
         if (!todos) todos = [];
         todos.forEach(item => {
             let name = item.name;
             let completed = item.completed;
-            let li = document.createTextNode(name);
+            let li = document.createElement("li");
+            let txt = document.createTextNode(name);
             li.appendChild(txt);
             if (completed) li.classList = "checked";
-            updatelistitem(li);
+            updateListItem(li);
             document.querySelector("#myUL").appendChild(li);
-        });
-    };
+        })
+    }
 
     function newElement() {
-
-
         let inputValue = document.querySelector("#myInput").value;
-
         if (inputValue.length > 0) {
             let li = document.createElement("li");
             li.innerHTML = inputValue;
             document.querySelector("#myUL").appendChild(li);
             document.querySelector("#myInput").value = "";
-            updatelistitem(li);
+            updateListItem(li);
 
             addNewTodo(inputValue);
             saveTodos();
@@ -57,41 +55,40 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             errorOn();
         }
-
     }
 
+    
     function errorOn() {
-        document.querySelector("#error").style.display = "flex";
+        document.querySelector("#error").style.display = "block";
     }
 
     function errorOff() {
-        document.querySelector("#error").style.display = "none";
+        document.querySelector(" #error").style.display = "none";
     }
 
-    function updatelistitem(item) {
-        let span = document.createElement('span');
-        let txt = document.createTextNode('\u00D7');
+    function updateListItem(item) {
+        let span = document.createElement("span");
+        let txt = document.createTextNode("\u00D7");
         span.className = "close";
         span.onclick = function () {
             this.parentElement.style.display = "none";
 
-            let index = todos.findIndex(i =>
-                i.name === (item.childNodes[0].textContent));
+            let index = todos.findIndex(i => i.name === (item.childNodes[0].textContent));
             todos.splice(index, 1);
             saveTodos();
         }
+
         span.appendChild(txt);
         item.appendChild(span);
         item.onclick = function () {
             this.classList.toggle("checked");
 
             console.log(this);
-            let temp = todos.find(i =>
-                i.name === (this.childNodes[0].textContent));
-
-                if(temp.completed) temp.completed = false;
-                else temp.completed = true;
-                saveTodos();
+            let temp = todos.find(i => i.name === (this.childNodes[0].textContent));
+            if (temp.completed) temp.completed = false;
+            else temp.completed = true;
+            saveTodos();
         }
     }
-});
+})
+;
